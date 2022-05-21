ea3037ba1d5cd4530989b28bb2253b76ace896a9
Tjark Ziehm <tjark.ziehm@ohioh.de>
2022-05-21 10:47:22 +0200

    built the app and added firts content

--- code/firstApplication/01-Name-Generator/app.vue
+++ code/firstApplication/01-Name-Generator/app.vue
@@ -1,5 +1,46 @@
 <template>
   <div>
-    <NuxtWelcome />
+    <h1>Name Generator</h1>
+    <p>Find a Name</p>
+
+    <div class="option-container-field">
+      <div class="option-container1">
+        <h4>1) Choose a gender</h4>
+        <div class="option-button-field"></div>
+        <div class="option-button">
+          <button>Boy</button>
+          <button>Girl</button>
+          <button>unisex</button>
+        </div>
+        <div class="option-button"></div>
+        <div class="option-button"></div>
+      </div>
+
+      <div class="option-container1">
+        <h4>2) Choose the name's popularity</h4>
+        <div class="option-button-field"></div>
+        <div class="option-button">
+          <button>conservative</button>
+          <button>neutral</button>
+          <button>fancy</button>
+        </div>
+        <div class="option-button"></div>
+        <div class="option-button"></div>
+      </div>
+
+      <div class="option-container1">
+        <h4>3) Choose the names length </h4>
+        <div class="option-button-field"></div>
+        <div class="option-button">
+          <button>Long</button>
+          <button>All</button>
+          <button>Short</button>
+        </div>
+        <div class="option-button"></div>
+        <div class="option-button"></div>
+
+      </div>
+
+    </div>
   </div>
 </template>
